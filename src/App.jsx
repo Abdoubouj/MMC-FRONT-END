ecd0d80f9453006ddb1bb43962c6d0361a38c40e
AbdouBouj <abderrahimboujedar@gmail.com>
2024-02-27 18:43:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+function App() {
+  return (
+    <div className='text-slate-700'>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nulla ea optio aspernatur quae facere consequatur placeat nam reprehenderit voluptatibus. Ipsum dolore voluptatibus laborum dolor quia cupiditate aliquid, repellendus voluptate!</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nulla ea optio aspernatur quae facere consequatur placeat nam reprehenderit voluptatibus. Ipsum dolore voluptatibus laborum dolor quia cupiditate aliquid, repellendus voluptate!</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nulla ea optio aspernatur quae facere consequatur placeat nam reprehenderit voluptatibus. Ipsum dolore voluptatibus laborum dolor quia cupiditate aliquid, repellendus voluptate!</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nulla ea optio aspernatur quae facere consequatur placeat nam reprehenderit voluptatibus. Ipsum dolore voluptatibus laborum dolor quia cupiditate aliquid, repellendus voluptate!</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nulla ea optio aspernatur quae facere consequatur placeat nam reprehenderit voluptatibus. Ipsum dolore voluptatibus laborum dolor quia cupiditate aliquid, repellendus voluptate!</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nulla ea optio aspernatur quae facere consequatur placeat nam reprehenderit voluptatibus. Ipsum dolore voluptatibus laborum dolor quia cupiditate aliquid, repellendus voluptate!</p>
+    </div>
+  )
+}
+
+export default App
